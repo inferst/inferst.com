3333c5b5497a842c66c17bbb307dde4a89abe6e1
Mikhail Morozov <inferst@gmail.com>
2025-07-16 19:20:51 +0300

    fix: build error

--- src/components/Face.tsx
+++ src/components/Face.tsx
@@ -84,7 +84,7 @@ const Face = () => {
           className="rounded-xl subpixel-antialiased"
           style={{ imageRendering: "pixelated" } as CSSProperties}
         />
-        <a
+        <Link
           href="/"
           className={`cursor-pointer block absolute top-0 left-0`}
           onMouseDown={handleMouseDown}
@@ -104,7 +104,7 @@ const Face = () => {
               } as CSSProperties
             }
           ></div>
-        </a>
+        </Link>
       </div>
     </div>
   );
